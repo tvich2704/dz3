ee54405da0642cd0d869cf34b7bba26e29a7a442
krasnobaev.a.a <tvich2704@gmail.com>
2022-12-18 14:45:41 +0500

    zadach19

---
+++ 3homwork/Program.cs
@@ -0,0 +1,17 @@
+﻿string line = Console.ReadLine();
+if (line.Length == 5)
+{
+    if (line[0] == line[4] && line[1] == line[3])
+    {
+        Console.WriteLine($"{line}->да");
+    }
+    else
+    {
+        Console.WriteLine($"{line}->нет");
+    }
+}
+else
+{
+    Console.WriteLine("число не пятизначное");
+}
+
