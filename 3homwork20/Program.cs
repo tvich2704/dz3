f58153085bd710b448b4ed019c837c84ce920c61
krasnobaev.a.a <tvich2704@gmail.com>
2022-12-18 15:20:48 +0500

    zadach

---
+++ 3homwork20/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.Write("Введите X1: ");
+int x1 = int.Parse(Console.ReadLine());
+Console.Write("Введите Y1: ");
+int y1 = int.Parse(Console.ReadLine());
+Console.Write("Введите Z1: ");
+int z1 = int.Parse(Console.ReadLine());
+Console.Write("Введите X2: ");
+int x2 = int.Parse(Console.ReadLine());
+Console.Write("Введите Y2: ");
+int y2 = int.Parse(Console.ReadLine());
+Console.Write("Введите Z2: ");
+int z2 = int.Parse(Console.ReadLine());
+
+double answer = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+
+Console.WriteLine($"d={answer:f3}");
+
---
+++ 3homwork23/Program.cs
@@ -0,0 +1,18 @@
+﻿int n = int.Parse(Console.ReadLine());
+Console.Write($"{n}->");
+if (n < 0)
+{
+    n = n * (-1);
+    for (int i = 1; i <= n; i++)
+    {
+        Console.Write($"-{Math.Pow(i, 3)} ");
+    }
+}
+else
+{
+    for (int i = 1; i <= n; i++)
+    {
+        Console.Write($"{Math.Pow(i, 3)} ");
+    }
+}
+
